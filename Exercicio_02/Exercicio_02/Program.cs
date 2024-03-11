57738925f8dc8cd36bd405ef6a8ee05ac201a99c
Murilo Vieira <murilo1.0@outlook.com>
2024-03-10 23:14:47 -0300

    initial commit

---
+++ Exercicio_02/Exercicio_02/Program.cs
@@ -0,0 +1,37 @@
+﻿class Program
+{
+    static void Main(String[] args)
+    {
+        Console.Write("Informe um número: ");
+        int n = int.Parse(Console.ReadLine());
+        if (isFibonacci(n))
+        {
+            Console.WriteLine($"O número {n} pertence a sequência de Fibonacci.");
+        }
+        else
+        {
+            Console.WriteLine($"O número {n} NÃO pertence a sequência de Fibonacci.");
+        }
+    }
+
+    static public bool isFibonacci(int n)
+    {
+        int a = 0;
+        int b = 1;
+        int i = 0;
+
+        while (i <= n)
+        {
+
+            if (i == n)
+            {
+                return true;
+            }
+
+            i = a + b;
+            a = b;
+            b = i;
+        }
+        return false;
+    }
+}
---
+++ Exercicio_05/Exercicio_05/Program.cs
@@ -0,0 +1,18 @@
+﻿class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine(reverseString("!kO tseT"));
+    }
+
+    static string reverseString(string str)
+    {
+        string reversedStr = "";
+
+        for (int i = str.Length - 1; i >= 0; i--)
+        {
+            reversedStr += str[i];
+        }
+        return reversedStr;
+    }
+}
